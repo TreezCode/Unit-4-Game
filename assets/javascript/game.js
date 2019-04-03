e92032e755bc834b8a3bda97129b8debf64babb7
TreezCode <TreezCode@users.noreply.github.com>
2019-04-03 12:08:41 -0600

    main logic update

---
+++ assets/javascript/game.js
@@ -0,0 +1,101 @@
+
+// GLOBAL VARIABLES
+// ===================================================================
+var compGuess = 0;
+var userScore = 0;
+var gem1 = 0;
+var gem2 = 0;
+var gem3 = 0;
+var gem4 = 0;
+var wins = 0;
+var losses = 0;
+var round = 1;
+var isDone = false;
+$("alert").html = message;
+var message = "";
+
+
+function reset() {
+   isDone = false;
+   compGuess = Math.floor(Math.random() * 102) + 19;
+   console.log(compGuess);
+   userScore = 0;
+   gem1 = Math.floor(Math.random() * 12) + 1;
+   gem2 = Math.floor(Math.random() * 12) + 1;
+   gem3 = Math.floor(Math.random() * 12) + 1;
+   gem4 = Math.floor(Math.random() * 12) + 1;
+   $("#target-score").html(compGuess);
+   $("#user-score").html(userScore);
+   $("#round").html(round);
+  
+}
+
+$(".btn").on("click", function start() {
+   reset();
+   $("#message").html("");
+});
+
+$("#gem1").on("click", function gemAdd() {
+   userScore = userScore + gem1;
+   $("#user-score").html(userScore);
+   $("#message").html("That crystal is worth " + gem1 + " !");
+   if (isDone === false) {
+      scoreCheck();
+   }
+});
+
+$("#gem2").on("click", function gemAdd() {
+   userScore = userScore + gem2;
+   $("#user-score").html(userScore);
+   $("#message").html("That crystal is worth " + gem2 + " !");
+   if (isDone === false) {
+      scoreCheck();
+   }
+});
+
+$("#gem3").on("click", function gemAdd() {
+   userScore = userScore + gem3;
+   $("#user-score").html(userScore);
+   $("#message").html("That crystal is worth " + gem3 + " !");
+   if (isDone === false) {
+      scoreCheck();
+   }
+});
+
+$("#gem4").on("click", function gemAdd() {
+   userScore = userScore + gem4;
+   $("#user-score").html(userScore);
+   $("#message").html("That crystal is worth " + gem4 + " !");
+   if (isDone === false) {
+      scoreCheck();
+   }
+});
+
+function scoreCheck() {
+   if (userScore === compGuess) {
+      $("#message").html("You Win!");
+      gem1 = 0;
+      gem2 = 0;
+      gem3 = 0;
+      gem4 = 0;
+      wins++;
+      round ++;
+      isDone = true;
+      $("#round").html(round);
+      $("#wins").html(wins);
+      reset();
+   }
+   if (userScore > compGuess) {
+      $("#message").html("Awe, maybe next time!");
+      gem1 = 0;
+      gem2 = 0;
+      gem3 = 0;
+      gem4 = 0;
+      losses++;
+      round++;
+      isDone = true;
+      $("#round").html(round);
+      $("#losses").html(losses);
+      reset();
+   }
+}
